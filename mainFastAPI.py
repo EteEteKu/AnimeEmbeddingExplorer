ac97551d23da9c8f49095c227e352656349d0615
eteeteku <vgilmiynov@inbox.ru>
2026-07-11 17:30:48 +0500

    изменение логики в поиске аниме, использование heapq

--- mainFastAPI.py
+++ mainFastAPI.py
@@ -5,20 +5,17 @@ import pandas as pd
 from searchSimAnime import search_anime
 DATAFRAME = pd.read_pickle(r"Animedataset\anime_processed.pkl")
 app = FastAPI()
-
 class AnimeRequest(BaseModel):
     anime_id:int 
     limit:int = 10
     min_similarity:float = 0.4
-
-
 @app.post("/anime/similar")
 def get_anime_similarity(request:AnimeRequest):
     similar = search_anime(
         request.anime_id,
-        10,
+        request.limit,
         DATAFRAME,
-        0.4
+        request.min_similarity
     )
     return {
 
--- searchSimAnime.py
+++ searchSimAnime.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from similarity import *
 from title_matching import *
+import heapq
 #model = SentenceTransformer('all-MiniLM-L6-v2')
 #def parse_genres(genre_string):
 #    return set(g for g in genre_string.split(","))
@@ -11,39 +12,30 @@ from title_matching import *
 #anime_pandas = anime_pandas.set_index('anime_id')
 #anime_pandas.to_pickle("anime_processed.pkl")
 
-def search_anime(anime_target:int, limit:int,dataframe:pd.DataFrame, min_similarity:float = 0.4):
+def build_anime_dict(anime_id:int, sim:float, dataframe:pd.DataFrame):
+    return{
+        "anime_id":anime_id,
+        "total_sim":float(sim), #np.float32 to float
+        "name": dataframe.at[anime_id,"Name"] if dataframe.at[anime_id,"English name"] == "UNKNOWN  " else dataframe.at[anime_id,"English name"],
+        "image_url":dataframe.at[anime_id,"Image URL"],
+        "genres":dataframe.at[anime_id,"Genres"],
+        "episodes": dataframe.at[anime_id,"Episodes"],
+        "score": dataframe.at[anime_id, "Score"]
+    }
+def search_anime(anime_target: int, limit: int, dataframe: pd.DataFrame, min_similarity: float = 0.4):
     all_anime_id = dataframe.index.tolist()
-    counter = limit
-    all_anime = []
+    heap = []
     for anime_id_2 in all_anime_id:
+        if anime_id_2 == anime_target:
+            continue
+        if is_related(dataframe.at[anime_target, "Name"], dataframe.at[anime_id_2, "Name"]):
+            continue
         total_sim = total_similarity(anime_target, anime_id_2, dataframe)
-        if total_sim > min_similarity and anime_target!=anime_id_2 and not is_related(dataframe.at[anime_target,"Name"], dataframe.at[anime_id_2,"Name"]):
-            counter-=1
-            all_anime.append({
-                "anime_id":anime_id_2,
-                "total_sim":float(total_sim), #np.float32 to float
-                "name": dataframe.at[anime_id_2,"Name"] if dataframe.at[anime_id_2,"English name"] == "UNKNOWN  " else dataframe.at[anime_id_2,"English name"],
-                "image_url":dataframe.at[anime_id_2,"Image URL"],
-                "genres":dataframe.at[anime_id_2,"Genres"],
-                "episodes": dataframe.at[anime_id_2,"Episodes"],
-                "score": dataframe.at[anime_id_2, "Score"]
-            }) 
-        if counter == 0: break
-    return all_anime
+        if total_sim > min_similarity:
+            heapq.heappush(heap, (total_sim, anime_id_2))
+            if len(heap) > limit:
+                heapq.heappop(heap)
+    top_n = sorted(heap, key=lambda x: x[0], reverse=True)
+    return [build_anime_dict(anime_id, sim, dataframe) for sim, anime_id in top_n]
 
 
-if __name__=="__main__":
-    anime_pandas = pd.read_pickle(r"Animedataset\anime_processed.pkl")
-    anime1 = 31240
-    #anime2 = 17265
-    recomend = search_anime(anime1,anime_pandas, 20)
-    if not recomend:
-        print("Не нашлось похожего тайтла")
-    else: 
-
-        for anime_id in recomend:
-            genre_score = genre_similarity(anime1, anime_id,anime_pandas)
-            desc_score = description_similarity(anime1, anime_id,anime_pandas)
-            sim = total_similarity(anime1,anime_id,anime_pandas)
-            name = anime_pandas.at[anime_id,"Name"]
-            print(f"{name}    {genre_score}    {desc_score}   {sim}")

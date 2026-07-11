16769ae1b7838a8030279901dc7111678a049a1d
eteeteku <vgilmiynov@inbox.ru>
2026-07-11 20:29:58 +0500

    начата разработка фронта

--- mainFastAPI.py
+++ mainFastAPI.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd 
 from searchSimAnime import search_anime
+from searchAnime import searchAnime
+from fastapi.middleware.cors import CORSMiddleware
+
 DATAFRAME = pd.read_pickle(r"Animedataset\anime_processed.pkl")
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  #
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class AnimeRequest(BaseModel):
     anime_id:int 
     limit:int = 10
@@ -24,4 +34,8 @@ def get_anime_similarity(request:AnimeRequest):
 
 @app.get("/anime/search")
 def get_search_anime(anime_name:str,limit:int=10):
-    ...
+    all_found_anime = searchAnime(anime_name,DATAFRAME,limit)
+    return {
+        "search_name":anime_name,
+        "found": all_found_anime
+    }
